7481a13661e375ef40bf594c56742df726942368
AnikDey-exe <deyanik2007@gmail.com>
2026-01-02 19:00:47 -0500

    added line

--- api/src/main/java/com/unihub/app/controller/HealthController.java
+++ api/src/main/java/com/unihub/app/controller/HealthController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
     @GetMapping("/health")
+    
     public ResponseEntity<String> health() {
         return ResponseEntity.ok("OK");
     }
