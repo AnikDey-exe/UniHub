00767d498af6978a04a13e792d4c2c35e8487e95
AnikDey-exe <deyanik2007@gmail.com>
2026-01-02 19:01:18 -0500

    removed line

--- api/src/main/java/com/unihub/app/controller/HealthController.java
+++ api/src/main/java/com/unihub/app/controller/HealthController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
     @GetMapping("/health")
-    
     public ResponseEntity<String> health() {
         return ResponseEntity.ok("OK");
     }
